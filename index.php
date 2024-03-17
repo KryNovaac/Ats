fbe2313191f298bbdf89ac833cb47ce08968f52d
KryNovaac <151487386+KryNovaac@users.noreply.github.com>
2024-03-17 17:57:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,113 +5,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Kelipatan Lima</title>
   <link rel="stylesheet" href="style.css" />
-  <style>
-    body {
-      background: cyan;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      min-height: 100vh; 
-      background: linear-gradient(90deg, rgb(135, 150, 255), rgb(206, 150, 255) 100%)
-    }
-
-    .m1 {
-        justify-content: center; 
-      color: white;
-      margin: 0 auto; 
-      width: 400px; 
-      padding: 20px;
-      border-radius: 20px; 
-      background-color: rgba(0, 0, 0, 0.7); 
-      margin:auto;
-      padding:auto;
-    }
-
-    .text {
-      font-size: 2em;
-      color: #fff;
-      text-align: center;
-    }
-
-    #judul{
-        text-align: center;
-        
-    }
-
-    .inputbox {
-      position: relative;
-      margin: 70px auto;
-      width: 310px;
-      border-bottom: 2px solid #fff;
-    }
-
-    .inputbox label {
-      position: absolute;
-      top: 50%;
-      left: 5px;
-      transform: translate(-50%);
-      color: #fff;
-      font-size: 1em;
-      pointer-events: none;
-      transition: 2.5s;
-      
-    }
-
-
-@keyframes fadein {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-    input:focus ~ label,
-    input:valid ~ label {
-      top: -5px;
-    }
-
-    .inputbox input {
-      width: 100%;
-      height: 50px;
-      background: transparent;
-      border: none;
-      outline: none;
-      font: 1em;
-      padding: 0 35px 0 5px;
-      color: #fff;
-    }
-
-    .m2 {
-      margin-top: 20px; 
-    }
-    .submit{
-        margin-left:170px;
-        border-radius: 20px; 
-        border:transparent;
-        color:white;
-        margin-top:-20px;
-    }
-
-    .m2{
-        transition: 2.5s;
-        background-color:white;
-        color:black;
-        border-radius:30px;
-        animation: fadein 2s ease-in-out;
-        margin:auto;
-        padding:auto;
-    }
-  </style>
+  <link rel="stylesheet" href="style1.css" />
+  <script src="js.js"></script>
 </head>
 <body>
-    <header>
-    <?php 
-    include "loading.php";
-    ?>
+<header>
+        <?php 
+       include "loading.php";
+        ?>
     </header>
-    
   <div class="m1">
     <div class="form-value">
       <form action="index.php" method="post" class="form-container">
@@ -143,6 +45,5 @@
       </div>
     </div>
   </div>
-  <script src="js.js"></script>
 </body>
 </html>
